5a55144f868fb957ad70bc10a87b61b0cc246077
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-08-15 15:09:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unicorked::Application.config.secret_key_base = 'c4ec044def05542f7a918eb513224ad60645dc0e28cbec871d5f8346797cefd3be9b1a036b7ebda1ce29c1e0beb26b820c11a99313a295ebc7c9b76b8114c7f7'
