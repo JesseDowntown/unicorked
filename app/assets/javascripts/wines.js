d23c20115151a20b21aa06a8bf742a82601384ac
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2014-08-18 00:09:54 -0500

    work in progress

--- app/assets/javascripts/wines.js
+++ app/assets/javascripts/wines.js
@@ -1,2 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+
+  $('.color-selector').change(function(){
+    var selectedColor = $("input:checked").val();
+
+    console.log('the color is:' + selected);
+  });
+
+});
